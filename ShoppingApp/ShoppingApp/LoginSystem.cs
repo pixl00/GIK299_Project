d4f5935cfb75b7ddb6268862431ee5814fcf3a43
Fredrikjanssonn <h25frjan@du.se>
2026-01-12 21:53:01 +0100

    Login + Users klasser addat

---
+++ ShoppingApp/ShoppingApp/LoginSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class LoginSystem
+{
+	private Dictionary<string, string> users = new Dictionary<string, string>()
+	{
+		{ "admin", "adminpass" }, 
+		{ "user2", "password2" },
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
